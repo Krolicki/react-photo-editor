b6bd5ec8f446847012890795a778a1ffde25311a
Jakub Królicki <jakub.krolicki.k@gmail.com>
2024-08-17 19:06:28 +0200

    changed unsplash api to picsum api

--- src/components/MainScreen/MainImage.tsx
+++ src/components/MainScreen/MainImage.tsx
@@ -6,7 +6,7 @@ type MainImageProps = {
 
 export const MainImage = ({setChoosenPhoto} : MainImageProps) => {
     const [imageLoaded, setImageLoaded] = useState(false)
-    const size = useRef(Math.floor(Math.random() * (500 - 300) + 300))
+    const size = useRef(Math.floor(Math.random() * 10 + 600))
 
     function handleImageLoad() {
         setImageLoaded(true)
@@ -15,7 +15,7 @@ export const MainImage = ({setChoosenPhoto} : MainImageProps) => {
         const canvas = document.createElement('canvas')
         const ctx = canvas.getContext('2d')
         canvas.width = size.current
-        canvas.height = 300
+        canvas.height = 600
         ctx?.drawImage(target as CanvasImageSource, 0, 0)
         canvas.toBlob(function(blob) {
             if(blob)
@@ -27,8 +27,9 @@ export const MainImage = ({setChoosenPhoto} : MainImageProps) => {
         <img
             className={imageLoaded ? "appear" : ""}
             crossOrigin="anonymous"
-            src={`https://source.unsplash.com/${size.current}x300/?random`}
-            alt='unsplash'
+            //src={`https://source.unsplash.com/${size.current}x300/?random`}
+            src={`https://picsum.photos/${size.current}/600`}
+            alt="random image from picsum"
             onClick={(e)=>handleClick(e)}
             onLoad={handleImageLoad}
         />
